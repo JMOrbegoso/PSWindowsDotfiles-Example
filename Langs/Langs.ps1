fcb3f73ebeaa35c64dd2038f5da3dbd263c78e5f
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2023-03-11 10:24:14 -0500

    Update Langs section

--- Langs/Langs.ps1
+++ Langs/Langs.ps1
@@ -1,1 +1,7 @@
 # Install & configure programming languages
+
+## Install Microsoft dotnet SDK
+if (-not (Test-WingetApp -AppId "Microsoft.DotNet.SDK.6")) {
+  Write-Host "Installing Microsoft DotNet SDK 6:" -ForegroundColor "Green";
+  Install-WingetApp -AppId "Microsoft.DotNet.SDK.6" -Source "winget";
+}

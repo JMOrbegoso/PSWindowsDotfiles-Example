0fcddca6f9be01a0898b8c8e874feb76694b5756
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2023-03-10 22:19:57 -0500

    Initial commit

---
+++ Dotfiles.ps1
@@ -0,0 +1,20 @@
+# Load Helpers
+Get-ChildItem "$env:DOTFILES_PATH\Helpers" -Filter *.ps1 | Foreach-Object { . $_.FullName; }
+
+# Install & configure base apps
+Invoke-Expression "$env:DOTFILES_PATH\Base\Base.ps1";
+
+# Install & configure programming languages
+Invoke-Expression "$env:DOTFILES_PATH\Langs\Langs.ps1";
+
+# Install & configure apps
+Invoke-Expression "$env:DOTFILES_PATH\Apps\Install.ps1";
+
+# Update & configure apps
+Invoke-Expression "$env:DOTFILES_PATH\Apps\Update.ps1";
+
+# Configure the Operating System
+Invoke-Expression "$env:DOTFILES_PATH\OS\OS.ps1";
+
+# Clean Dotfiles temp folder & clean the Operating System
+Invoke-Expression "$env:DOTFILES_PATH\Clean\Clean.ps1";

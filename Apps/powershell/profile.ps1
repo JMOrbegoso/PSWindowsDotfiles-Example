67395571e4098b0f8b5090693c943bd11a572dc0
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2023-03-11 19:09:10 -0500

    Use agnoster.minimal theme

--- Apps/powershell/profile.ps1
+++ Apps/powershell/profile.ps1
@@ -15,7 +15,7 @@ Get-ChildItem "$DOTFILES_PATH\Shell\scripts" -Filter *.ps1 | Foreach-Object { . 
 Get-ChildItem "$DOTFILES_PATH\Shell\functions" -Filter *.ps1 | Foreach-Object { . $_.FullName; }
 
 # Load OhMyPosh
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\iterm2.omp.json" | Invoke-Expression; # --config "$DOTFILES_PATH\Shell\themes\agnoster.omp.json"
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\agnoster.minimal.omp.json" | Invoke-Expression; # --config "$DOTFILES_PATH\Shell\themes\agnoster.omp.json"
 
 # Winfetch
 pwshfetch-test-1;
